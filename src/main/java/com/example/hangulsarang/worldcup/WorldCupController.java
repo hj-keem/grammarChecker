4dbd93c8de1cb3b6d0188e4bc673a93bdcdcf585
hj-keem <hyeomjeom@gmail.com>
2024-07-19 13:21:27 +0900

    Controller 수정

--- src/main/java/com/example/hangulsarang/worldcup/WorldCupController.java
+++ src/main/java/com/example/hangulsarang/worldcup/WorldCupController.java
@@ -8,23 +8,23 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequiredArgsConstructor
 public class WorldCupController {
-    private final WorldCupTestService testService;
+    private final WorldCupService service;
 
     // 후보 정보를 나타내는 로직
     @GetMapping("/candidates")
     public String getCandidates() throws JsonProcessingException {
-        return testService.getCandidate();
+        return service.getCandidate();
     }
 
     // 사용자가 선택한 후보를 나타내는 로직
     @PostMapping("/selectedCandidate")
     public String getSelectedCandidate(@RequestBody CandidateDto dto) throws JsonProcessingException {
-        return testService.getSelectedCandidate(dto);
+        return service.getSelectedCandidate(dto);
     }
 
     // 다음 라운드로 넘어가는 로직
     @PostMapping("/proceedToNextRound")
     public void proceedToNextRound() {
-        testService.proceedToNextRound();
+        service.proceedToNextRound();
     }
 }
