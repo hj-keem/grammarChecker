813da9228e21ed48d59b0320a8595d53fd9da5bc
hj-keem <hyeomjeom@gmail.com>
2024-08-04 18:39:01 +0900

    게시물 검색기능 추가

--- src/main/java/com/example/hangulsarang/notice/repository/PostRepository.java
+++ src/main/java/com/example/hangulsarang/notice/repository/PostRepository.java
@@ -1,8 +1,13 @@
 package com.example.hangulsarang.notice.repository;
 
 import com.example.hangulsarang.notice.entity.PostEntity;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface PostRepository extends JpaRepository<PostEntity, Long> {
+    Page<PostEntity> findByTitleContaining(String keyword, Pageable pageable);
 
 }
