8e9efe4eef62dbd7f21adc83bf80b1eb70a221e8
hj-keem <hyeomjeom@gmail.com>
2024-08-08 02:14:30 +0900

    정적 리소스 경로 지정

--- src/main/java/com/example/hangulsarang/WebConfig.java
+++ src/main/java/com/example/hangulsarang/WebConfig.java
@@ -11,6 +11,14 @@ public class WebConfig implements WebMvcConfigurer {
         // /media/img/ 경로로 요청을 받았을 때 실제 파일 시스템의 media/img/ 디렉토리로 리소스 처리
         registry.addResourceHandler("/static/img/**")
                 .addResourceLocations("file:media/img/");
+        // 정적 리소스
+        registry.addResourceHandler("/js/**")
+                .addResourceLocations("classpath:/static/js/")
+                .setCachePeriod(3600); // 캐시 기간 설정
+        registry.addResourceHandler("/css/**")
+                .addResourceLocations("classpath:/static/css/")
+                .setCachePeriod(3600); // 캐시 기간 설정
+
     }
 }
 
