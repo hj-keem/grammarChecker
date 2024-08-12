bdacfde7acb0a15dbe0367c52f8edde248e0d7a5
hj-keem <hyeomjeom@gmail.com>
2024-08-12 14:35:09 +0900

    (Oauth) 클라이언트에서 받아온 uri 저장

---
+++ src/main/java/com/example/hangulsarang/RedirectUriRequest.java
@@ -0,0 +1,13 @@
+package com.example.hangulsarang;
+
+public class RedirectUriRequest {
+    private String redirectUri;
+
+    public String getRedirectUri() {
+        return redirectUri;
+    }
+
+    public void setRedirectUri(String redirectUri) {
+        this.redirectUri = redirectUri;
+    }
+}
