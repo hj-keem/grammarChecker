0f54fd068a82bc369a6905d87c9d2a2064a30250
hj-keem <hyeomjeom@gmail.com>
2024-08-12 14:35:33 +0900

    (Oauth) 로그인 처리 설정

---
+++ src/main/java/com/example/hangulsarang/SecurityConfig.java
@@ -0,0 +1,30 @@
+package com.example.hangulsarang;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+// 최신 Spring Security에서 OAuth 2.0 로그인을 구성
+public class SecurityConfig {
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception { //HTTP 요청에 대한 보안 설정을 적용
+        http
+                .csrf(AbstractHttpConfigurer::disable) // CSRF 보호 비활성화
+                .authorizeHttpRequests(authorizeRequests ->
+                        authorizeRequests
+                                .anyRequest().permitAll() // 모든 요청을 허용
+                )
+                // OAuth 2.0을 사용하여 로그인을 처리하는 설정
+                .oauth2Login(oauth2Login ->
+                        oauth2Login
+                                .defaultSuccessUrl("/handleLoginSuccess", true)
+                                .failureUrl("/login?error")
+                );
+        return http.build();
+    }
+}
